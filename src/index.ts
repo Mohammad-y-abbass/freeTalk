be49278177e95a946b5731e574b6899427b5b73d
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-02 20:06:54 +0300

    create connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+express.urlencoded({ extended: false });
+express.json();
+
+const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error('Please define a MONGO_URI environment variable');
+  }
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err: any) => {
+      console.log('Error connecting to MongoDB', err);
+    });
+};
+
+start();
+
 app.listen(3001, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
